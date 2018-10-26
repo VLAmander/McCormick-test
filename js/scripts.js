7fea17df69425322690e1c5f2e4a0b56b28b9220
VLAmander <clouddavis0296@yahoo.com>
2018-10-25 20:51:14 -0500

    new js and view able password eye

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+   let $passwordInput = $("#passwordInput");
+  let $showHidePassword = $("#showHidePassword");
+  let $eyeCon = $("#eyeCon");
+   console.log(showHidePassword);
+   $showHidePassword.on('click', function(event){
+    event.preventDefault();
+    console.log("eyeball was clicked");
+     if ($passwordInput.attr("type") == "password") {
+      console.log("type is password")
+      $eyeCon.removeClass("fa-eye-slash");
+      $eyeCon.addClass("fa-eye");
+      $passwordInput.attr("type", "text");
+    } else {
+      $eyeCon.removeClass("fa-eye");
+      $eyeCon.addClass("fa-eye-slash");
+      $passwordInput.attr("type", "password");
+    }
+  });
+ });
